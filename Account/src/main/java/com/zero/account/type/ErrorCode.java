9470d7b89b3d8e3018a0020218669c738b8afa72
heesane <tees3359@gmail.com>
2024-06-17 22:58:16 +0900

    Error Code 추가

--- Account/src/main/java/com/zero/account/type/ErrorCode.java
+++ Account/src/main/java/com/zero/account/type/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     ACCOUNT_NOT_FOUND("계좌가 존재하지 않습니다."),
     USER__ACCOUNT_UNMATCHED("사용자와 계좌의 주인이 일치하지 않습니다."),
     ACCOUNT_ALREADY_UNREGISTERED("계좌가 이미 비활성 상태입니다."),
-    BALANCE_NOT_EMPTY("계좌 잔액이 0이 아닙니다.");
+    BALANCE_NOT_EMPTY("계좌 잔액이 0이 아닙니다."),
+    AMOUNT_EXCEED_BALANCE("출금액이 잔액을 초과합니다.");
 
     private final String description;
 }
