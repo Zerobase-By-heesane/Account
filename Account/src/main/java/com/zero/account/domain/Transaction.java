7a229052eea12559718f23149c313d068e6fc30e
heesane <tees3359@gmail.com>
2024-06-17 22:58:40 +0900

    Transaction Entity

---
+++ Account/src/main/java/com/zero/account/domain/Transaction.java
@@ -0,0 +1,51 @@
+package com.zero.account.domain;
+
+import com.zero.account.type.AccountStatus;
+import com.zero.account.type.TransactionResultType;
+import com.zero.account.type.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+
+    @ManyToOne
+    private Account account;
+
+    private Long amount;
+
+    private Long balanceSnapshot;
+
+    private String transactionId;
+
+    private LocalDateTime transactedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
