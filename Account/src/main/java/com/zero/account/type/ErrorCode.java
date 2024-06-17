1278efd0e4610a1e960abdd40624696b6635e3ef
heesane <tees3359@gmail.com>
2024-06-17 18:07:30 +0900

    ErrorCode 예외 처리를 위한 enum 추가

--- Account/src/main/java/com/zero/account/type/ErrorCode.java
+++ Account/src/main/java/com/zero/account/type/ErrorCode.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자가 없습니다.");
+    USER_NOT_FOUND("사용자가 없습니다."),
+    MAX_ACCOUNT_PER_USER_10("사용자당 계좌는 최대 10개까지 생성 가능합니다.")
+    ;
 
     private final String description;
 }
