4318a8e08555558b2fb80863853f5a4fe04ed8fd
heesane <tees3359@gmail.com>
2024-06-17 22:59:18 +0900

    Transaction Repository

---
+++ Account/src/main/java/com/zero/account/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.zero.account.repository;
+
+import com.zero.account.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+
+}
