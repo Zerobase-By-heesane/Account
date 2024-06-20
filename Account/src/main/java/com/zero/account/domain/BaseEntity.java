ee332dd1498d3b3d8a47f177cc92d6aa292b7140
heesane <tees3359@gmail.com>
2024-06-20 22:26:23 +0900

    Refactoring

---
+++ Account/src/main/java/com/zero/account/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.zero.account.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName="doesNotUseThisBuilder")
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
